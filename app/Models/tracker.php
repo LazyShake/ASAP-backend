45f0cccf9115c001b354e0fbfdc58a11da18b4cb
LazyShake <sashaoligator@gmail.com>
2024-12-19 19:08:39 +0700

    renamed some classes

--- app/Models/tracker.php
+++ app/Models/tracker.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class tracker extends Model
+class Tracker extends Model
 {
     use HasFactory;
-    
+
     protected $table = 'trackers';
 
     // Первичный ключ
