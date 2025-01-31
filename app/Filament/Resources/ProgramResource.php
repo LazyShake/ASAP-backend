4d9cce05f8c2eb807c55e6b22bdcdb3e495448d4
LazyShake <sashaoligator@gmail.com>
2025-01-31 19:37:21 +0700

    changed attributes

--- app/Filament/Resources/ProgramResource.php
+++ app/Filament/Resources/ProgramResource.php
@@ -37,7 +37,6 @@ class ProgramResource extends Resource
                             ->maxLength(255),
                         Forms\Components\TextInput::make('text')
                             ->label('Текст')
-                            ->required()
                             ->maxLength(255),
                         Forms\Components\Repeater::make('sub_items')
                             ->label('Подэлементы')
