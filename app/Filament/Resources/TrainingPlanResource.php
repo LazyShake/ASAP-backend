1eb13a76448ca14a3e1b30050e9f02c0d68a9931
LazyShake <sashaoligator@gmail.com>
2025-02-04 23:57:59 +0700

    added folders

--- app/Filament/Resources/TrainingPlanResource.php
+++ app/Filament/Resources/TrainingPlanResource.php
@@ -25,7 +25,6 @@ class TrainingPlanResource extends Resource
                 Forms\Components\FileUpload::make('image')
                     ->label('Изображение')
                     ->image()
-                    ->imagePreviewHeight(150)
                     ->preserveFilenames()
                     ->disk('public') // Указываем диск
     ->directory('training_plan') ,
