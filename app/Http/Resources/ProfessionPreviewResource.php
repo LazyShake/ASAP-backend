e148ae468c8cd4cbdc32f3c70a4f4f6fb0006587
LazyShake <sashaoligator@gmail.com>
2025-02-05 14:33:42 +0000

    Revert "Merge branch 'to-merge' into 'dev'"
    This reverts merge request !135

--- app/Http/Resources/ProfessionPreviewResource.php
+++ app/Http/Resources/ProfessionPreviewResource.php
@@ -4,7 +4,6 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use App\Models\Tariff;
 
 class ProfessionPreviewResource extends JsonResource
 {
@@ -24,7 +23,6 @@ class ProfessionPreviewResource extends JsonResource
             'price' => $this->price,
             'type' => $this->typeProfession->name_type,
             'color' => $this->color->name,
-            'tariff' => new TariffResource(Tariff::orderByDesc('price')->first()),
         ];
     }
 }
