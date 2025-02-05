3261e24c3f18d1cbd80cabc43c994cb6eb1b9e13
LazyShake <sashaoligator@gmail.com>
2025-02-05 23:27:17 +0700

    changed plural for first image

--- app/Http/Resources/ProfessionGeneralResource.php
+++ app/Http/Resources/ProfessionGeneralResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Tariff;
 
 class ProfessionGeneralResource extends JsonResource
 {
--- app/Http/Resources/ProfessionResource.php
+++ app/Http/Resources/ProfessionResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Carbon\Carbon;
+use App\Models\Tariff;
 
 class ProfessionResource extends JsonResource
 {
