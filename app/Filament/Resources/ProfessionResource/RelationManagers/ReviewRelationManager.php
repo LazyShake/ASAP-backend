f11a9de7caa256782c7d03541cb5049886b5664f
LazyShake <sashaoligator@gmail.com>
2025-02-05 15:55:56 +0700

    deleted factorys and updated seeders

--- app/Filament/Resources/ProfessionResource/RelationManagers/ReviewRelationManager.php
+++ app/Filament/Resources/ProfessionResource/RelationManagers/ReviewRelationManager.php
@@ -22,7 +22,7 @@ class ReviewRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('text')
+                Forms\Components\TextArea::make('text')
                     ->label('Текст отзыва')
                     ->required(),
                 Forms\Components\FileUpload::make('picture')
