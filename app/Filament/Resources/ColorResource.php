66c9a0390307f0ab8a1dbde00c88cea05e6413b8
LazyShake <sashaoligator@gmail.com>
2025-02-08 21:31:45 +0700

    fixed color

--- app/Filament/Resources/ColorResource.php
+++ app/Filament/Resources/ColorResource.php
@@ -2,7 +2,7 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\ColorResource\Pages;
-use App\Models\color;
+use App\Models\Color;
 use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
