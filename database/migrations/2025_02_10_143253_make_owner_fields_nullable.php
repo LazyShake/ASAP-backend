f8fdcfe0103ebea518fb8a4bf68d8ef0473c88a7
LazyShake <sashaoligator@gmail.com>
2025-02-10 21:33:17 +0700

    fixed requirements

---
+++ database/migrations/2025_02_10_143253_make_owner_fields_nullable.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->text('owner_name')->nullable()->change();
+            $table->text('owner_description')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->text('owner_name')->nullable(false)->change();
+            $table->text('owner_description')->nullable(false)->change();
+        });
+    }
+};
