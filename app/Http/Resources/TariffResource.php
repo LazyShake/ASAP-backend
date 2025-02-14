47fd035d7de350e5145323e782341c6bf9f8cb81
LazyShake <sashaoligator@gmail.com>
2025-02-14 15:55:39 +0700

    formated price and installment

--- app/Http/Resources/TariffResource.php
+++ app/Http/Resources/TariffResource.php
@@ -13,7 +13,7 @@ class TariffResource extends JsonResource
             'name_tariff' => $this->name_tariff,
             'short_description' => $this->short_description,
             'price' => number_format($this->price, 0, '.', ' '),
-            'instalment' => number_format((int) preg_replace('/\D/', '', $this->instalment), 0, '.', ' '),
+            'instalment' => number_format($value, 0, '.', ' '),
 
             'detailed_description' => $this->detailed_description,
         ];
